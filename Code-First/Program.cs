7ecd4dbb86ecd761c8da2957a0210fff4cb8d690
Abdullah Şentürk <asenturk22@yandex.com>
2024-12-23 00:37:31 +0300

    Add project files.

---
+++ Code-First/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Code_First
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Kod üzerinden migrate işlemi. 
+            ECommerceDbContext context = new ECommerceDbContext();
+            await context.Database.MigrateAsync();
+        }
+    }
+
+    // Context 
+    public class ECommerceDbContext : DbContext
+    {
+        public DbSet<Product> Proudcts { get; set; } // Product Tablosu 
+        public DbSet<Customer> Customers { get; set; } // Customer Tablosu 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-G27PV8J\\SQLEXPRESS; initial Catalog=ECommerceDb; integrated Security=true; ");
+        }
+    }
+
+
+    // Entity
+    public class Product
+    {
+        public int Id { get; set; }         // Product.Id sütunu
+        public string Name { get; set; }    // Product.Name sütunu
+        public int Quantity { get; set; }   // Product.Quantity sütunu
+        public float Price { get; set; }    // Product.Price sütunu
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }                 // Customer.Id
+        public string FirstName { get; set; }       // Customer.FirstName
+        public string LastName { get; set; }        // Customer.LastName
+
+    }
+}
+
+
+/*
+PMC : Tools
+PM> add-migration mig1
+Build started...
+Build succeeded.
+
+veya 
+CLI : Design  paketi yüklenmeli 
+C:\ECommerce> dotnet ef migrations add mig1
+
+
+*/
---
+++ Database-First/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Database_First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
+/*
+ PMC komut aracılığıyla . 
+
+PMC> Scaffold-DbContext 
+       "Server=localhost\SQLEXPRESS;Database=NORTHWND;Integrated Security=True;" 
+       Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
+ */
